b7c4d039cced3ed36dba0b56d22d5c5b7e49d2dd
codernike <108070610+codernike@users.noreply.github.com>
2022-07-24 16:08:15 +0530

    time

---
+++ src/routes/time.svelte
@@ -0,0 +1,19 @@
+<script>
+ 
+    let  date = new Date();
+ let time = date.toLocaleTimeString();
+    
+
+</script>
+<div class="clocky" style="margin-top: 180px;text-align:center;">
+<center>   <div id="MyClockDisplay" class="clock" >{time}</div></center> 
+</div>
+
+<style>
+    .clock{
+        width: 50%;
+        border: 3px rgb(9, 218, 250) solid;
+        font-size:8em;
+        font-weight: 600;
+    }
+</style>
